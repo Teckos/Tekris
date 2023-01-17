0529d0ca242a5899e241d696a96ae8ae59d1c59b
Teckos <teckos@hotmail.fr>
2023-01-17 15:01:41 +0100

    Update MainView.swift

--- Tekris/MainView.swift
+++ Tekris/MainView.swift
@@ -14,46 +14,52 @@ struct MainView: View {
     @EnvironmentObject var board: Board
     @State var isPresented = false
     @State var Pause = false
-    
     var bgColor: Double {
         return board.level < 10 ? Double(board.level)/10 : 1
     }
+    
     var body: some View {
         ZStack {
             Background(bgColor: bgColor)
             VStack{
                 ScoresView(size: 12.0)
+                    .onTapGesture {
+//A tap on the scores triggers the pause
+                        Pause = true
+                    }
                 Spacer()
             }
             VStack {
                 Spacer()
                 Spacer()
+//Board drawing
                 BlankBoard()
                     .onAppear(perform: {
+//The game starts right as the board is drawn
                         board.NewBrick()
                     })
-                    .onTapGesture {
-                        Pause = true
-                    }
                 Spacer()
                 Buttons(board: board)
                     .padding(.bottom)
             }
             .alert("Recommencer ?", isPresented: $board.gameOver) {
                 Button("Oui") {
+//Alert is shown when gameOver is true. When "Oui" is clicked, the board resets along with all the scores and a new game starts.
                     board.reset()
                     board.NewBrick()
                 }
             }
             .sheet(isPresented: $Pause) {
+//Modal pause view call
                 PauseView().opacity(0.7)
             }
         }
     }
 }
 
+//Changing the background gradient's colors dynamically based on the property bgColor which is a computed property that changes according to the player's level.
 struct Background: View {
-    var bgColor: Double
+    let bgColor: Double
     var body: some View {
         LinearGradient(gradient: Gradient(colors: [ Color(red: bgColor, green: 1 - bgColor, blue: 1 - bgColor), Color(red: 1 - bgColor, green: 1 - bgColor, blue: bgColor), Color(red: bgColor, green: 1 - bgColor, blue: bgColor)]), startPoint: .bottomTrailing, endPoint: .topLeading)
             .foregroundColor(.white)
@@ -62,11 +68,11 @@ struct Background: View {
     }
 }
 
+//Basic modal view that pauses the game and displays the scores
 struct PauseView: View {
     var body: some View {
         ZStack{
             Background(bgColor: 0.9)
-            
             VStack{
                 Text("Breaktime")
                     .font(.system(size: 30, weight: .bold))
